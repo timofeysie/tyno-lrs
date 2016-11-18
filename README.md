1f79b604be28d5c5ad967e63d87c903e7214ccdb
Timothy Curchod <timofeyc@hotmail.com>
2016-11-19 07:04:58 +1100

    Step 5: add routes

--- README.md
+++ README.md
@@ -4,22 +4,23 @@ Follow along with your friends and family with [the tut](http://mherman.org/blog
 
 
 ## Step 1: Typescript
-
+```
 $ touch tsconfig.json
 $ mkdir src
 $ npm init -y
 ()
 $ npm install typescript@2.0.6 --save-dev
 $ node_modules/.bin/tsc
-
+```
 
 ## Step 2: Gulp
-
+```
 $ npm install gulp@3.9.1 gulp-typescript@3.1.1 --save-dev
 $ touch gulpfile.json
+```
 
 ## Step 3: Express
-
+```
 $ npm install express@4.14.0 debug@2.2.0 --save
 $ npm install @types/node@6.0.46 @types/express@4.0.33 @types/debug@0.0.29 --save-dev
 $ touch src/index.tsc
@@ -28,14 +29,23 @@ $ npm install express@4.14.0 body-parser@1.15.2 morgan@1.7.0 --save
 $ npm install @types/body-parser@0.0.33 @types/morgan@1.7.32 --save-dev
 $ gulp scripts
 $ npm start
+```
 
 ## Step 4: TDD
-
+```
 $ npm install mocha@3.1.2 chai@3.5.0 chai-http@3.0.0 --save-dev
 $ npm install @types/mocha@2.2.32 @types/chai@3.4.34 @types/chai-http@0.0.29 --save-dev
 $ npm install ts-node@1.6.1 --save-dev
 $ touch test/helloWorld.test.ts
 $ npm test
+```
 
+## Step 5: Add Routes
+```
+// add some more tests.
+$ touch src/data.json
+$ touch src/routes/HeroRourter.ts
+$ npm test
+```
 
 
--- test/helloWorld.test.ts
+++ test/helloWorld.test.ts
@@ -4,20 +4,54 @@ import chaiHttp = require('chai-http');
 import app from '../src/App';
 chai.use(chaiHttp);
 const expect = chai.expect;
-describe('baseRoute', () => {
+describe('Express API tests', () => {
 
-  it('should be json', () => {
-    chai.request(app).get('/')
-    .then(res => {
-      expect(res.type).to.eql('application/json');
+  describe('baseRoute', () => {
+    it('should be json', () => {
+        chai.request(app).get('/')
+        .then(res => {
+        expect(res.type).to.eql('application/json');
+        });
+    });
+
+    it('should have a message prop', () => {
+        chai.request(app).get('/')
+        .then(res => {
+        expect(res.body.message).to.eql('Hello World!');
+        });
     });
   });
 
-  it('should have a message prop', () => {
-    chai.request(app).get('/')
-    .then(res => {
-      expect(res.body.message).to.eql('Hello World!');
+  describe('GET api/v1/heroes', () => {
+    it('responds with JSON array', () => {
+        return chai.request(app).get('/api/v1/heroes')
+        .then(res => {
+            expect(res.status).to.equal(200);
+            expect(res).to.be.json;
+            expect(res.body).to.be.an('array');
+            expect(res.body).to.have.length(5);
+        });
     });
-  });
+
+    it('should include Wolverine', () => {
+        return chai.request(app).get('/api/v1/heroes')
+        .then(res => {
+            let Wolverine = res.body.find(hero => hero.name === 'Wolverine');
+            expect(Wolverine).to.exist;
+            expect(Wolverine).to.have.all.keys([
+            'id',
+            'name',
+            'aliases',
+            'occupation',
+            'gender',
+            'height',
+            'hair',
+            'eyes',
+            'powers'
+            ]);
+        });
+    });
+    
+});
 
 });
